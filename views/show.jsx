c2999ccc857ca11cccd2eeb429e203393fff13fd
charlie reyes <charliereyes1228@gmail.com>
2022-11-29 21:19:01 -0800

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -6,8 +6,8 @@ function Show ({bread, index}) {
     //console.log(bread.name)
         return (
             <Default>
-                <h2>Show Page</h2>
-                <h3>{bread.name}</h3>
+                <h2>{bread.name}</h2>
+                {/* <h3>{bread.name}</h3> */}
                 <p>
                     and it
                     {
@@ -19,10 +19,11 @@ function Show ({bread, index}) {
                 </p>
                 <img src={bread.image} alt={bread.name} />
                 <li><a href="/breads">Go home</a></li>
-                    
-<form action={`/breads/${index}?_method=DELETE`} method="POST">
-  <input type='submit' value="DELETE"/>
-</form>
+                <form action={`/breads/${index}?_method=DELETE`} method="POST">
+                     <input type='submit' value="DELETE"/>
+                </form>
+                <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+
 
 
             </Default>
